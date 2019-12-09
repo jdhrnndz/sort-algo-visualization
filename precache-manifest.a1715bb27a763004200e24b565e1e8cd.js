d469a4fae99585d40e06d30fc208885111adeffd
jdhrnndz <johndeniellehernandez@gmail.com>
2019-12-09 02:59:32 +0000

    Updates

--- precache-manifest.a1715bb27a763004200e24b565e1e8cd.js
+++ precache-manifest.a1715bb27a763004200e24b565e1e8cd.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e641d99f64d148344a9e30b06328de08",
+    "revision": "c82f9a7b1cc3f60a242ca241c2258c7c",
     "url": "/sort-algo-visualization/index.html"
   },
   {
-    "revision": "8e4958275a15d132f7ba",
-    "url": "/sort-algo-visualization/static/css/main.0392fceb.chunk.css"
+    "revision": "ae615ccc9107abd17b72",
+    "url": "/sort-algo-visualization/static/css/main.c674cf14.chunk.css"
   },
   {
     "revision": "e688af86e27935b9c240",
     "url": "/sort-algo-visualization/static/js/2.e4de7b87.chunk.js"
   },
   {
-    "revision": "8e4958275a15d132f7ba",
-    "url": "/sort-algo-visualization/static/js/main.d9f791df.chunk.js"
+    "revision": "ae615ccc9107abd17b72",
+    "url": "/sort-algo-visualization/static/js/main.8f5cc099.chunk.js"
   },
   {
     "revision": "744a0b9be4d11b6c6bde",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sort-algo-visualization/precache-manifest.9ae09204b4538831393280c71ef97549.js"
+  "/sort-algo-visualization/precache-manifest.a1715bb27a763004200e24b565e1e8cd.js"
 );
 
 self.addEventListener('message', (event) => {
