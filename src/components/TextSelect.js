e8c5da0c7b0cde323b2769727637cf2a3d785980
JD Hernandez <johndeniellehernandez@gmail.com>
2023-06-21 18:05:49 +0800

    create TextSelect component

---
+++ src/components/TextSelect.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function renderOptions(options, setValue) {
+    return Object.entries(options).map(([key, value]) => (
+        <span className="dropdown__item" key={key} onClick={() => setValue(key)}>{value.title}</span>
+    ));
+}
+
+function TextSelect(props) {
+    const [expanded, setExpanded] = React.useState(false);
+
+    return (
+        <div className={`dropdown ${expanded && 'dropdown--expanded'}`} onClick={() => setExpanded(!expanded)}>
+            <span className="dropdown__item--selected">{props.options[props.value].title}</span>
+            {
+                expanded
+                &&
+                <div className="dropdown__options">
+                    {renderOptions(props.options, props.setValue)}
+                </div>
+            }
+        </div>
+    );
+}
+
+export default TextSelect;
