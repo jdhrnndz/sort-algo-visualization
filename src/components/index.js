a1d4b8df45a6d1d060e81add430ce5e1ecb5eee6
JD Hernandez <johndeniellehernandez@gmail.com>
2023-06-21 18:06:46 +0800

    organize components module exports

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Animation } from './Animation';
+export { default as CircleCanvas } from './CircleCanvas';
+export { default as HexCanvas } from './HexCanvas';
+export { default as SquareCanvas } from './SquareCanvas';
+export { default as TextSelect } from './TextSelect';
